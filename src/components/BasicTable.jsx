c77cc9bec7d7db3a2fcdee645e1111255097b816
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-08-01 13:15:05 +0300

    very basic case

---
+++ src/components/BasicTable.jsx
@@ -0,0 +1,47 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import MOCK_DATA from "../data/MOCK_DATA.json";
+import { COLUMNS } from "../data/column";
+
+export const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const tableInstance = useTable({
+    columns: columns,
+    data: data,
+  });
+
+  const { getTableBodyProps, getTableProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <div>
+      <table {...getTableProps}>
+        <thead>
+          {headerGroups.map((item) => (
+            <tr {...item.getHeaderGroupProps()}>
+              {item.headers.map((col) => (
+                <th {...col.getHeaderProps}>{col.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
---
+++ src/data/column.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "Email",
+    accessor: "email",
+  },
+  {
+    Header: "County",
+    accessor: "country",
+  },
+  {
+    Header: "Age",
+    accessor: "age",
+  },
+  {
+    Header: "Phone",
+    accessor: "phone",
+  },
+];
